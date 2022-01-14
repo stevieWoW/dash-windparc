d2c62c0bd12deac9c5a0219ff443e13407fd387e
stevieWoW <stefantoebben@gmx.de>
2022-01-14 22:17:33 +0000

    add server = app.server

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ app = dash.Dash(__name__, meta_tags=[
         {"name": "viewport", "content": "width=device-width, initial-scale=1"}
     ])
 
-
+server = app.server
 
 
 ##################################################
